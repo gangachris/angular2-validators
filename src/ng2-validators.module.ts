787f769da02977eae6af6a996ee3be7d894ec9c9
Ganga Chris <ganga.chris@gmail.com>
2016-09-19 13:14:54 +0300

    Add more validators

--- src/ng2-validators.module.ts
+++ src/ng2-validators.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 
 import {
   EmailValidator,
-  AlphaValidator
+  AlphaValidator,
+  AlphanumericValidator,
+  AsciiValidator,
+  Base64Validator
 } from './ng2-validators';
 
 @NgModule({
-  declarations: [EmailValidator, AlphaValidator],
-  exports: [EmailValidator, AlphaValidator]
+  declarations: [EmailValidator, AlphaValidator, AlphanumericValidator, AsciiValidator, Base64Validator],
+  exports: [EmailValidator, AlphaValidator, AlphanumericValidator, AsciiValidator, Base64Validator]
 })
 export class Ng2ValidatorsModule{ }
--- src/ng2-validators.ts
+++ src/ng2-validators.ts
@@ -1,2 +1,5 @@
 export * from './validators/is-email';
 export * from './validators/is-alpha';
+export * from './validators/is-alphanumeric';
+export * from './validators/is-ascii';
+export * from './validators/is-base64';
