95ae9b44b86c40a7336f23a878833d633a5e43cc
Ganga Chris <ganga.chris@gmail.com>
2016-09-20 01:46:27 +0300

    More Validators

--- src/ng2-validators.module.ts
+++ src/ng2-validators.module.ts
@@ -1,17 +1,68 @@
 import { NgModule } from '@angular/core';
 
 import {
-  EmailValidator,
+  AfterValidator,
   AlphaValidator,
   AlphanumericValidator,
   AsciiValidator,
   Base64Validator,
-  AfterValidator,
-  BooleanValidator
+  BeforeValidator,
+  BooleanValidator,
+  ByteLengthValidator,
+  CreditCardValidator,
+  CurrencyValidator,
+  DataUriValidator,
+  DateValidator,
+  DecimalValidator,
+  EmailValidator,
+  FloatValidator,
+  FQDNValidator,
+  HexColorValidator,
+  HexaDecimalValidator,
+  IntValidator,
+  IPValidator,
+  ISBNValidator,
+  JSONValidator,
+  LowerCaseValidator,
+  MacAddressValidator,
+  NullValidator,
+  NumericValidator,
+  UpperCaseValidator,
+  URLValidator
 } from './ng2-validators';
 
+let Validators = [
+  AfterValidator,
+  AlphaValidator,
+  AlphanumericValidator,
+  AsciiValidator,
+  Base64Validator,
+  BeforeValidator,
+  BooleanValidator,
+  ByteLengthValidator,
+  CreditCardValidator,
+  CurrencyValidator,
+  DataUriValidator,
+  DateValidator,
+  DecimalValidator,
+  EmailValidator,
+  FloatValidator,
+  FQDNValidator,
+  HexColorValidator,
+  HexaDecimalValidator,
+  IntValidator,
+  IPValidator,
+  ISBNValidator,
+  JSONValidator,
+  LowerCaseValidator,
+  MacAddressValidator,
+  NullValidator,
+  NumericValidator,
+  UpperCaseValidator,
+  URLValidator]
+
 @NgModule({
-  declarations: [EmailValidator, AlphaValidator, AlphanumericValidator, AsciiValidator, Base64Validator, AfterValidator, BooleanValidator],
-  exports: [EmailValidator, AlphaValidator, AlphanumericValidator, AsciiValidator, Base64Validator, AfterValidator, BooleanValidator]
+  declarations: Validators,
+  exports: Validators
 })
-export class Ng2ValidatorsModule{ }
+export class Ng2ValidatorsModule { }
