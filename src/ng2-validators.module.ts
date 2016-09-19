8fb5c281eceb77dbf706f937ff0ebeb95e318113
Margaret Ochieng <margaret.ochieng@andela.com>
2016-09-19 15:25:59 +0300

    Add is-after validator

--- src/ng2-validators.module.ts
+++ src/ng2-validators.module.ts
@@ -5,11 +5,12 @@ import {
   AlphaValidator,
   AlphanumericValidator,
   AsciiValidator,
-  Base64Validator
+  Base64Validator,
+  AlphaValidator
 } from './ng2-validators';
 
 @NgModule({
-  declarations: [EmailValidator, AlphaValidator, AlphanumericValidator, AsciiValidator, Base64Validator],
-  exports: [EmailValidator, AlphaValidator, AlphanumericValidator, AsciiValidator, Base64Validator]
+  declarations: [EmailValidator, AlphaValidator, AlphanumericValidator, AsciiValidator, Base64Validator, AlphaValidator],
+  exports: [EmailValidator, AlphaValidator, AlphanumericValidator, AsciiValidator, Base64Validator, AlphaValidator]
 })
 export class Ng2ValidatorsModule{ }
--- src/ng2-validators.ts
+++ src/ng2-validators.ts
@@ -3,3 +3,4 @@ export * from './validators/is-alpha';
 export * from './validators/is-alphanumeric';
 export * from './validators/is-ascii';
 export * from './validators/is-base64';
+export * from './validators/is-after';
