aada468a649eadae8799d34d439a19483d024b3f
Ganga Chris <ganga.chris@gmail.com>
2016-09-22 00:10:24 +0300

    Fix equals validators

--- src/validators/equals.ts
+++ src/validators/equals.ts
@@ -1,13 +1,13 @@
 import { Directive, forwardRef, Attribute } from '@angular/core';
 import { NG_VALIDATORS, Validator, AbstractControl } from '@angular/forms';
 
-import { getValidator, getDirectiveName, getDirectiveProviders } from './helpers';
+import { getDirectiveName, getDirectiveProviders, getValidatorWithParam } from './helpers';
 
 import * as validator from 'validator';
 
-const name = 'isEqual';
+const name = 'equals';
 
-export const equals = getValidator(name)
+export const equals = getValidatorWithParam(name)
 
 @Directive({
   selector: getDirectiveName(name),
@@ -16,12 +16,12 @@ export const equals = getValidator(name)
   ]
 })
 export class EqualsValidator implements Validator {
-  constructor( @Attribute('isEqual') public equals: string) { }
+  constructor( @Attribute(name) public param: string) { }
 
   validate(c: AbstractControl): { [key: string]: any } {
-    if (this.equals && !validator.equals(c.value, this.equals))
+    if (this.param && !validator[name](c.value, this.param))
       return {
-        equal: false
+        [name]: false
       }
     return null;
   }
